0f4309d12524c02a34f6e1793f3aa1b2ece49381
CodeLucho33 <code.lucho33@gmail.com>
2025-03-24 02:36:52 -0500

    first commit

---
+++ src/test/java/com/codeluchoro33/API_movie_reservation/ApiMovieReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeluchoro33.API_movie_reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiMovieReservationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
